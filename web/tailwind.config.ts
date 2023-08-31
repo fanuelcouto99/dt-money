9a2c058e8f82ac8aebf2100e25d166264def78a0
Fanuel Couto <fanuel.couto99@gmail.com>
2023-08-30 22:27:26 -0300

    STYLE[Layout]: Configurado o style da aplicação, setando cores padrões a serem usados e fontes

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from 'tailwindcss'
+/** @type {import('tailwindcss').Config} */
 
-const config: Config = {
+module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,13 +8,36 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sansRegular: 'var(--font-roboto-400)',
+        sansBold: 'var(--font-roboto-700)',
       },
+      colors: {
+        green: {
+          300: '#00B37E',
+          500: '#00875F',
+          600: '#015F43'
+        },
+        red: {
+          300: '#F75A68',
+          500: '#AB222E',
+          700: '#7A1921'
+        },
+        gray: {
+          100: '#E1E1E6',
+          300: '#C4C4CC',
+          400: '#8D8D99',
+          500: '#7C7C8A',
+          600: '#323238',
+          700: '#29292E',
+          800: '#202024',
+          900: '#121214',
+        },
+        yellow: {
+          500: '#FBA94C'
+        }
+      }
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
-export default config
